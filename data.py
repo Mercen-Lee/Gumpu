c9946115f27ddbcdcc7e0710725aca84a350f8a9
Mercen <mercen@mercen.net>
2023-01-29 22:48:08 +0900

    ADD: data.py

---
+++ data.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class DB:
+    # Class Initialization
+    def __init__(self, id: int):
+        self.id = id
+
+        # SQLite Database Connection
+        self.connection = sqlite3.connect("data.db", isolation_level=None)
+        self.cursor = self.connection.cursor()
+
+        # Create table if not exist
+        self.cursor.execute("""CREATE TABLE if not Exists Gumpu
+               (ID INTEGER, Coins INTEGER, BTC INTEGER, ETH INTEGER, Date INTEGER)""")
+        self.cursor.execute(f"SELECT EXISTS(SELECT Coins FROM Gumpu WHERE ID={id})")
+
+        # Create row if not exist
+        if not self.cursor.fetchone()[0]:
+            self.cursor.execute(f"INSERT INTO Gumpu VALUES({id}, 0, 0, 0, 0)")
+
+    # Fetching Data
+    def fetch(self, key: str) -> int:
+        self.cursor.execute(f"SELECT {key} FROM Gumpu WHERE ID={self.id}")
+        return self.cursor.fetchone()[0]
+
+    # Updating Data
+    def update(self, key: str, value: str):
+        self.cursor.execute(f"UPDATE Gumpu SET {key}={value} WHERE ID={self.id}")
+
+    # Auto-closing SQLite Database
+    def __exit__(self):
+        self.connection.close()
--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-import os, config, discord, sqlite3, time, random
-
-class DB:
-    # Class Initialization
-    def __init__(self, id: int):
-        self.id = id
-
-        # SQLite Database Connection
-        self.connection = sqlite3.connect("data.db", isolation_level=None)
-        self.cursor = self.connection.cursor()
-
-        # Create table if not exist
-        self.cursor.execute("""CREATE TABLE if not Exists Gumpu
-               (ID INTEGER, Coins INTEGER, BTC INTEGER, ETH INTEGER, Date INTEGER)""")
-        self.cursor.execute(f"SELECT EXISTS(SELECT Coins FROM Gumpu WHERE ID={id})")
-
-        # Create row if not exist
-        if not self.cursor.fetchone()[0]:
-            self.cursor.execute(f"INSERT INTO Gumpu VALUES({id}, 0, 0, 0, 0)")
-
-    # Fetching Data
-    def fetch(self, key: str) -> int:
-        self.cursor.execute(f"SELECT {key} FROM Gumpu WHERE ID={self.id}")
-        return self.cursor.fetchone()[0]
-
-    # Updating Data
-    def update(self, key: str, value: str):
-        self.cursor.execute(f"UPDATE Gumpu SET {key}={value} WHERE ID={self.id}")
-
-    # Auto-closing SQLite Database
-    def __exit__(self):
-        self.connection.close()
+import config, discord, data, time, random
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -58,10 +27,10 @@ async def on_message(message):
     async def helper(msg: str):
         await send(f":warning: {msg}", "이렇게 해달라멍!", color=0xFF0000)
 
-    def fmt(num): return format(int(num), ',')
+    def fmt(num): return format(int(num), ",")
 
     async def betting(money):
-        data = DB(author)
+        data = data.DB(author)
         current = data.fetch("Coins")
 
         if current <= 0 or current < money:
@@ -69,11 +38,11 @@ async def on_message(message):
             return
         elif random.choice([True, False]):
             data.update("Coins", current + money)
-            title = ':slight_smile: 성공했다멍!'
+            title = ":slight_smile: 성공했다멍!"
             description = f"`+{fmt(money)} 코인`"
         else:
             data.update("Coins", current - money)
-            title = ':cry: 실패했다멍!'
+            title = ":cry: 실패했다멍!"
             description = f"`-{fmt(money)} 코인`"
 
         await send(title, description)
@@ -84,37 +53,49 @@ async def on_message(message):
 
     try:
         if math[author] == int(message.content):
-            money = random.randint(3000, 5000)
+            money = random.randint(1000, 2000)
 
             del math[author]
             timeleft[author] = round(time.time())
 
-            data = DB(author)
+            data = data.DB(author)
             data.update("Coins", data.fetch("Coins") + money)
 
-            await send(':thumbsup: 정답이다멍!', description=f"`+{fmt(money)} 코인`")
+            await send(":thumbsup: 정답이다멍!", description=f"`+{fmt(money)} 코인`")
             return
     except: pass
 
-    msg = message.content.split(' ')
-    if msg[0] not in ['검푸야', 'ㄱㅍ']: return
+    msg = message.content.split(" ")
+    if msg[0] not in config.command: return
 
-    data = DB(author)
+    data = data.DB(author)
 
-    if msg[1] in ['버전', 'ㅂㅈ']:
-        title = ':bone: 2023.1.26 Poodle v0.2'
-        description = 'Gumpu Developed by Mercen Lee'
+    # Admin Commands
+    if msg[1] in ["돈내놔", "ㄷㄴㄴ"]:
+        if message.author.id == config.admin:
+            try:
+                data.update("Coins", data.fetch("Coins") + int(msg[2]))
+                await send(":cry: 내놨다멍!", description=f"`+{fmt(int(msg[2]))} 코인`")
+            except:
+                await helper("검푸야 돈내놔 [금액]")
+        else:
+            await send(":warning: 너는 내 주인이 아니다멍!", color=0xFF0000)
+
+    # Normal Commands
+    if msg[1] in ["버전", "ㅂㅈ"]:
+        title = ":bone: 2023.1.26 Poodle v0.2"
+        description = "Gumpu Developed by Mercen Lee"
         await send(title, description)
 
-    if msg[1] in ['돈', 'ㄷ']:
+    if msg[1] in ["돈", "ㄷ"]:
         title = f":coin: {fmt(data.fetch('Coins'))} 코인"
         description = []
         for t in ["BTC", "ETH"]:
             fetched = data.fetch(t)
             if fetched: description.append(f"` {fmt(data.fetch(t))} {t}`")
-        await send(title, ', '.join(description))
+        await send(title, ", ".join(description))
 
-    if msg[1] in ['돈받기', 'ㄷㅂㄱ']:
+    if msg[1] in ["돈받기", "ㄷㅂㄱ"]:
         if author in list(timeleft.keys()):
             left = round(time.time()) - timeleft[author]
             if left < 60:
@@ -124,19 +105,19 @@ async def on_message(message):
         num = []
         for x in range(0, 2): num.append(random.randint(1, 10))
 
-        temp = {'+': num[0] + num[1], '-': num[0] - num[1], '×': num[0] * num[1]}
+        temp = {"+": num[0] + num[1], "-": num[0] - num[1], "×": num[0] * num[1]}
         chosen = random.choice(list(temp.keys()))
 
         math[author] = temp[chosen]
         
         await send(f":regional_indicator_q: {num[0]} {chosen} {num[1]}", color=0x5287BE)
 
-    if msg[1] in ['올인', 'ㅇㅇ']: await betting(data.fetch("Coins"))
+    if msg[1] in ["올인", "ㅇㅇ"]: await betting(data.fetch("Coins"))
 
-    if msg[1] in ['하프', 'ㅎㅍ']: await betting(data.fetch("Coins") // 2)
+    if msg[1] in ["하프", "ㅎㅍ"]: await betting(data.fetch("Coins") // 2)
 
-    if msg[1] in ['도박', 'ㄷㅂ']:
+    if msg[1] in ["도박", "ㄷㅂ"]:
         try: await betting(int(msg[2]))
-        except: await helper('검푸야 도박 [금액]')
+        except: await helper("검푸야 도박 [금액]")
 
 client.run(token)
